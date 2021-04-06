aba2236cedad94aac475a241f2468ec260cf90b6
Jordan Hamlin <Jordan.Hamlin6@gmail.com>
2021-04-06 11:23:09 +1000

    Added script creation functionality into ContextMenu.

--- com.jordantama.settings/Editor/SettingsContextMenu.cs
+++ com.jordantama.settings/Editor/SettingsContextMenu.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
+﻿using System.IO;
 using UnityEditor;
 using UnityEditor.ProjectWindowCallback;
-using UnityEngine;
 using Object = UnityEngine.Object;
 using ContextMenu = EditorUtils.ContextMenu;
 
@@ -23,30 +20,11 @@ namespace Settings
         [MenuItem(ContextDirectory + "Settings")]
         private static void NewSettings()
         {
-            CreateCreateRuntimeScripts(SettingsPath,
+            CreateScriptFromTemplate<DoCreateSettingsScript>(SettingsPath,
                 "New" + Path.GetFileNameWithoutExtension(SettingsPath) + ".cs");
         }
 
-
-        private static void CreateCreateRuntimeScripts(string templatePath, string defaultNewFileName)
-        {
-            // Exceptions
-            if (templatePath == null)
-                throw new ArgumentNullException(nameof(templatePath));
-            
-            if (!File.Exists(templatePath))
-                throw new FileNotFoundException($"The template file \"{templatePath}\" could not be found.", templatePath);
-
-            if (string.IsNullOrEmpty(defaultNewFileName))
-                defaultNewFileName = Path.GetFileName(templatePath);
-            
-            // Asset icon
-            Texture2D icon = EditorGUIUtility.IconContent("cs Script Icon").image as Texture2D;
-
-            DoCreateSettingsScript onNameEditEnd = ScriptableObject.CreateInstance<DoCreateSettingsScript>();
-            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, onNameEditEnd, defaultNewFileName, icon, templatePath);
-        }
-
+        
         private static Object CreateSettingsScript(string pathName)
         {
             string content = File.ReadAllText(SettingsPath);
@@ -74,7 +52,10 @@ namespace Settings
             content = content.Replace("#SCRIPTNAME#", scriptName);
             content = content.Replace("#SETTINGSNAME#", baseFileNoSpaces);
 
-            return CreateScriptAssetWithContent(settingsPathName.Replace(baseFile, scriptName), content);
+            string path = settingsPathName.Remove(settingsPathName.Length - Path.GetFileName(settingsPathName).Length) +
+                          Path.GetFileName(settingsPathName).Replace(baseFile, scriptName);
+            
+            return CreateScriptAssetWithContent(path, content);
         }
 
         private static Object CreateSettingsWindowScript(string settingsPathName)
@@ -91,52 +72,12 @@ namespace Settings
 
             string directory = Path.GetDirectoryName(Path.GetFullPath(settingsPathName));
             Directory.CreateDirectory(directory + "/Editor");
+
+            string path =
+                settingsPathName.Remove(settingsPathName.Length - Path.GetFileName(settingsPathName).Length) +
+                "Editor/" + Path.GetFileName(settingsPathName).Replace(baseFile, scriptName);
             
-            return CreateScriptAssetWithContent(settingsPathName.Replace(baseFile, "Editor/" + scriptName), content);
-        }
-
-        private static Object CreateScriptAssetWithContent(string pathName, string content)
-        {
-            content = SetLineEndings(content, EditorSettings.lineEndingsForNewScripts);
-
-            string fullPath = Path.GetFullPath(pathName);
-            var encoding = new System.Text.UTF8Encoding(true);
-
-            File.WriteAllText(fullPath, content, encoding);
-            AssetDatabase.ImportAsset(pathName);
-
-            return AssetDatabase.LoadAssetAtPath(pathName, typeof(Object));
-        }
-        
-        private static string SetLineEndings(string content, LineEndingsMode lineEndingsMode)
-        {
-            const string windowsLineEndings = "\r\n";
-            const string unixLineEndings = "\n";
-
-            string preferredLineEndings;
-
-            switch (lineEndingsMode)
-            {
-                case LineEndingsMode.OSNative:
-                    if (Application.platform == RuntimePlatform.WindowsEditor)
-                        preferredLineEndings = windowsLineEndings;
-                    else
-                        preferredLineEndings = unixLineEndings;
-                    break;
-                case LineEndingsMode.Unix:
-                    preferredLineEndings = unixLineEndings;
-                    break;
-                case LineEndingsMode.Windows:
-                    preferredLineEndings = windowsLineEndings;
-                    break;
-                default:
-                    preferredLineEndings = unixLineEndings;
-                    break;
-            }
-
-            content = Regex.Replace(content, @"\r\n?|\n", preferredLineEndings);
-
-            return content;
+            return CreateScriptAssetWithContent(path, content);
         }
 
         
--- com.jordantama.tools/Editor/ContextMenu.cs
+++ com.jordantama.tools/Editor/ContextMenu.cs
@@ -1,5 +1,10 @@
-using UnityEditor;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using Object = UnityEngine.Object;
 
 namespace EditorUtils
 {
@@ -19,10 +24,82 @@ namespace EditorUtils
             ProjectWindowUtil.CreateAsset(asset, path);
         }
         
-        // TODO: Create generic function to create a script from a template (more flexible than existing methods)
-        protected static void CreateScriptFromTemplate(string template)
+        /// <summary>
+        /// Create a new script from a template.
+        /// </summary>
+        /// <param name="templatePath">The path of the template script file.</param>
+        /// <param name="defaultName">The default name of the new script file.</param>
+        /// <typeparam name="T">The type of <see cref="EndNameEditAction"/> used to conclude name editing.</typeparam>
+        /// <exception cref="ArgumentNullException">The template path is null.</exception>
+        /// <exception cref="FileNotFoundException">The template path is invalid.</exception>
+        protected static void CreateScriptFromTemplate<T>(string templatePath, string defaultName = null)
+            where T : EndNameEditAction
         {
+            // Exceptions
+            if (templatePath == null)
+                throw new ArgumentNullException(nameof(templatePath));
+
+            if (!File.Exists(templatePath))
+                throw new FileNotFoundException($"The template file {templatePath} could not be found.", templatePath);
+
+            if (string.IsNullOrEmpty(defaultName))
+                defaultName = Path.GetFileName(templatePath);
             
+            // Asset icon
+            Texture2D icon = EditorGUIUtility.IconContent("cs Script Icon").image as Texture2D;
+
+            T onNameEditEnd = ScriptableObject.CreateInstance<T>();
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, onNameEditEnd, defaultName, icon, templatePath);
+        }
+        
+        /// <summary>
+        /// Create a script with content.
+        /// </summary>
+        /// <param name="pathName">The path to create the script at, relative to the project directory.</param>
+        /// <param name="content">The content to populate the new script with.</param>
+        /// <returns>The created script as a <see cref="UnityEngine.Object"/>.</returns>
+        protected static Object CreateScriptAssetWithContent(string pathName, string content)
+        {
+            content = SetLineEndings(content, EditorSettings.lineEndingsForNewScripts);
+
+            string fullPath = Path.GetFullPath(pathName);
+            var encoding = new System.Text.UTF8Encoding(true);
+
+            File.WriteAllText(fullPath, content, encoding);
+            AssetDatabase.ImportAsset(pathName);
+
+            return AssetDatabase.LoadAssetAtPath(pathName, typeof(Object));
+        }
+        
+        private static string SetLineEndings(string content, LineEndingsMode lineEndingsMode)
+        {
+            const string windowsLineEndings = "\r\n";
+            const string unixLineEndings = "\n";
+
+            string preferredLineEndings;
+
+            switch (lineEndingsMode)
+            {
+                case LineEndingsMode.OSNative:
+                    if (Application.platform == RuntimePlatform.WindowsEditor)
+                        preferredLineEndings = windowsLineEndings;
+                    else
+                        preferredLineEndings = unixLineEndings;
+                    break;
+                case LineEndingsMode.Unix:
+                    preferredLineEndings = unixLineEndings;
+                    break;
+                case LineEndingsMode.Windows:
+                    preferredLineEndings = windowsLineEndings;
+                    break;
+                default:
+                    preferredLineEndings = unixLineEndings;
+                    break;
+            }
+
+            content = Regex.Replace(content, @"\r\n?|\n", preferredLineEndings);
+
+            return content;
         }
     }
 }
