40f8d1842c15b2bbb0eebe8bac77fc0794627bc5
JordanTama <51806398+JordanTama@users.noreply.github.com>
2021-08-25 23:09:37 +1000

    Added Service Locator.

---
+++ com.jordantama.service-locator/Runtime/Locator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ServiceLocator
+{
+    public class Locator
+    {
+        private readonly Dictionary<string, IService> services = new Dictionary<string, IService>();
+
+        private static Locator instance;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            instance = new Locator();
+            
+            Type[] types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes())
+                .Where(p => typeof(IService).IsAssignableFrom(p) && p != typeof(IService)).ToArray();
+
+            foreach (Type type in types)
+            {
+                dynamic service = Convert.ChangeType(Activator.CreateInstance(type), type);
+                Register(service);
+            }
+        }
+
+        public static T Get<T>() where T : IService
+        {
+            string key = typeof(T).Name;
+
+            if (!instance.services.ContainsKey(key))
+                throw new Exception($"{key} is not a registered service.");
+
+            return (T) instance.services[key];
+        }
+
+        public static void Register<T>(T service) where T : IService
+        {
+            string key = typeof(T).Name;
+            
+            if (instance.services.ContainsKey(key))
+            {
+                Debug.LogError($"Attempted to register service of type {key} which has already been registered.");
+                return;
+            }
+
+            instance.services.Add(key, service);
+        }
+
+        public static void Unregister<T>() where T : IService
+        {
+            string key = typeof(T).Name;
+
+            if (!instance.services.ContainsKey(key))
+            {
+                Debug.LogError($"Attempted to unregister service of type {key} which is not registered.");
+                return;
+            }
+
+            instance.services.Remove(key);
+        }
+    }
+}
